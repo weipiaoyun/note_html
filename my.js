cc903952e454d1103c41a30fb096598a01f30f6f
weipiaoyun <525921240@qq.com>
2019-12-19 20:26:54 +0800

    'update'

--- my.js
+++ my.js
@@ -110,14 +110,27 @@ var backButton=$('.back_to_top');
     }
     backButton.on('click', backToTop);
  
+var div1=document.getElementById('back_to_topA');
     $(window).on('scroll', function () {/*当滚动条的垂直位置大于浏览器所能看到的页面的那部分的高度时，回到顶部按钮就显示 */
+		//滚动的时候检测，如果电脑端，直接返回
+		var nnn = url.search(/mysite/);
+		if(nnn > 0){
+			div1.style.display = "none";
+			return;
+		} else {
+		} 
+		alert("aaaa");
+
         if ($(window).scrollTop() > $(window).height())
             backButton.fadeIn();
         else
             backButton.fadeOut();
+
+
     });
 
 
+/*返回首页根据地址不同返回不同*/
 var div=document.getElementById('returnIndexA');
 	 var url = window.location.href;
 	 var nnn = url.search(/weipiaoyun/);
@@ -129,4 +142,10 @@ var div=document.getElementById('returnIndexA');
 
 
 
+/*隐藏返回顶部， 手机用的*/
+var nnnn = url.search(/mysite/);
+if(nnnn > 0){
+	backButton.fadeOut();
+	div1.style.display = "none";
+} 
 
